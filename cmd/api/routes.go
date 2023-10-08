634d20e61e6cd2e84dde15e7cb5ff014dc89f697
Isaac Peña <info@ipenat.com>
2023-10-08 19:25:53 +0200

    bug fixed in routes.go

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
@@ -16,6 +16,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
-	// return app.recoverPanic(router)
-	return router
+	return app.recoverPanic(router)
 }
