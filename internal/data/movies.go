d5ab874de04d7c853198c8abd9d144a86f06dbb8
Isaac Peña <info@ipenat.com>
2023-10-09 23:43:11 +0200

    Query in Update function has been updated

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -56,7 +56,7 @@ func (m MovieModel) Update(movie *Movie) error {
 	query := `
 		UPDATE movies
 		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-		WHERE id = $5
+		WHERE id = $5 AND version = $6
 		RETURNING version`
 
 	args := []any{
