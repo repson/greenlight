46fb1492761bbee5f7c2fc47c0e41a2393020671
Isaac Peña <info@ipenat.com>
2023-10-09 22:36:35 +0200

    Added field validation in updateMovieHandler function

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -107,10 +107,21 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie.Title = *input.Title
-	movie.Year = *input.Year
-	movie.Runtime = *input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
