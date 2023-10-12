926006a83671ef471c30713b6f27c5b264e63ea6
Isaac Peña <info@ipenat.com>
2023-10-12 20:49:41 +0200

    A new GetAll method has been created that returns multiple records

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -155,3 +155,48 @@ func (m MovieModel) Delete(id int64) error {
 
 	return nil
 }
+
+func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
+	query := `
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		ORDER BY id`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	row, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer row.Close()
+
+	movies := []*Movie{}
+
+	for row.Next() {
+		var movie Movie
+
+		err := row.Scan(
+			&movie.ID,
+			&movie.CreatedAt,
+			&movie.Title,
+			&movie.Year,
+			&movie.Runtime,
+			pq.Array(&movie.Genres),
+			&movie.Version,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		movies = append(movies, &movie)
+	}
+
+	if err = row.Err(); err != nil {
+		return nil, err
+	}
+
+	return movies, nil
+}
