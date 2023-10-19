ba936524c669a75e11730a7920a131874869d754
Isaac Peña <info@ipenat.com>
2023-10-19 22:08:52 +0200

    Added a goroutine in server.go to catch the signals

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -17,7 +20,19 @@ func (app *application) serve() error {
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
-	app.logger.Info("starting server on %s", srv.Addr, "env", app.config.env)
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		app.logger.Info("caught signal", "signal", s.String())
+
+		os.Exit(0)
+	}()
+
+	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
 
 	return srv.ListenAndServe()
 }
