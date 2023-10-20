1dedc0ab3a2147a37802ab43a61738f197ee0738
Isaac Peña <info@ipenat.com>
2023-10-20 23:33:43 +0200

    validators created in users.go

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"time"
 
+	"github.com/repson/greenlight/internal/validator"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -12,7 +14,7 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	Password  string    `json:"-"`
+	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int32     `json:"version"`
 }
@@ -46,4 +48,30 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
